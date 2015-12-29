7c4e17e166d4ff7c84c12aa4825510acd873811c
Luigi Bennardis <l.bennardis@email.it>
2015-12-29 16:16:51 +0100

    Test

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+		//test 
 		SpringApplication.run(Application.class, args);
 	}
 
